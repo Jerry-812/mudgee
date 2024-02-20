de6fa44f9650f7ac6f5e3f8bc7c983a3c47c434b
Jerry-812 <jx2498@nyu.edu>
2024-02-20 16:12:55 +0800

    update

--- joy-master/joy-master/.muse/setup.sh
+++
@@ -1,5 +0,0 @@
-if [ $(whoami) = "root" ]; then
-    apt install -y libpcap-dev
-fi
-
-cd $1 ; ./configure
--- joy-master/joy-master/safe_c_stub/include/mem_primitives_lib.h
+++
@@ -1,4 +0,0 @@
-#ifndef __MEM_PRIMITIVES_LIB__
-#define __MEM_PRIMITIVES_LIB__
-
-#endif // __MEM_PRIMITIVES_LIB__
